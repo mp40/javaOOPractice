6fceefa4c3572d1fa7b1a7ecf0ad1876ede0ef75
Matthew Philps <philps.matthew@gmail.com>
2020-03-01 16:45:56 +0900

    set up project

---
+++ src/udemyRocketSimulator/SimulateRockets.java
@@ -0,0 +1,4 @@
+package udemyRocketSimulator;
+
+public class SimulateRockets {
+}
---
+++ tests/udemyRocketSimulator/SimulateRocketsTests.java
@@ -0,0 +1,13 @@
+package udemyRocketSimulator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class SimulateRocketsTests {
+
+    @Test
+    public void simulateRockers() {
+      //  fail();
+    }
+}
