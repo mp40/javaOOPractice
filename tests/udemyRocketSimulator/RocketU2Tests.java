14cd4f19ae6ad66e2475f907a87896aa6311f32c
Matthew Philps <philps.matthew@gmail.com>
2020-03-14 18:44:12 +0900

    added rocket methods and item object

--- tests/udemyRocketSimulator/RocketU2Tests.java
+++ tests/udemyRocketSimulator/RocketU2Tests.java
@@ -3,7 +3,6 @@ package udemyRocketSimulator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RocketU2Tests {
 
